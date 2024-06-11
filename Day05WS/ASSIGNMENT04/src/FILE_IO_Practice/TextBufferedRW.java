2ce72e0c9cc077f6cf30765c0f00743b87a14c45
Kunal Verma <kv47871@gmail.com>
2024-06-11 12:13:02 +0530

    Changing their name and formating

--- Day05WS/ASSIGNMENT04/src/FILE_IO_Practice/TextBufferedRW.java
+++ Day05WS/ASSIGNMENT04/src/FILE_IO_Practice/TextBufferedRW.java
@@ -1,78 +1,85 @@
-package FILE_IO_Practice;
+package FILE_IO;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.*;
+import java.util.Scanner;
 
 public class TextBufferedRW {
-	private static final String PATH_NAME = "Demoo.txt";
-	// Aim to read or Write the multiple bit of data inside the binary file
-	// 1 Extra Step Adding of BufferedOutputStream & BufferedInputStream
-	// As they are Abstract Classes Named as - FileOutputStream or FileInputStream
-	// This Abstract Classes are Extended from the Real I/OStream Files 
-	
-	static Scanner sc = new Scanner(System.in);
-	
-	public static int menuList(){		
-		System.out.println("-------------------------------------------------------------"
-				+ "\nEnter the Option:\n0. Exit\n1. Write By TBH\n2. Read By TBH");
-		int choice = sc.nextInt();
-		if (choice==0) {
-			System.out.println("Exited...");
-		}
-		return choice;
-	}
-	
-// Code of writeRecord() & readRecord() WITH Try Block Handler
-	public static void TBH_writeRecord() throws FileNotFoundException, IOException{
-		// TRY BLOCK HANDLER Will take some resource from the Try Block and can't contain the catch block
-		// As we provided the source to the Try block we don't have to write anything extra for handle the exception as it throws automatically
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File(PATH_NAME)))){
-			for (char ch = 'A';ch < 'z'; ch++) {
-				bw.write(ch);
-			}
-		}
-	}
-	
-	public static void TBH_readRecord() throws FileNotFoundException, IOException{
-		try(BufferedReader br = new BufferedReader(new FileReader(new File(PATH_NAME)))){
-			int data;
-			while ((data = br.read())!=-1) {
-				System.out.print((char)data+" ");
-			}
-			System.out.println();
-		}
-		// not have to worry about the catch block
-	}
+    static Scanner sc = new Scanner(System.in);
+    static BookManagement bookManagement = new BookManagement();
 
-	
-	
+    public static int menuList() {
+        System.out.println("-------------------------------------------------------------"
+                + "\nEnter the Option:\n0. Exit\n1. Display All Books\n2. Find Book\n3. Add New Book\n4. Remove Book\n5. Edit Book Quantity\n6. Save and Exit");
+        int choice = sc.nextInt();
+        sc.nextLine(); // consume newline
+        return choice;
+    }
 
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-//		System.out.println("Hello Guys!!");
-		int choice;
-		while ((choice = menuList())!=0){
-			switch (choice) {
-			case 1:
-				TBH_writeRecord();
-				break;
-			case 2:
-				TBH_readRecord();
-				break;
-			default:
-				System.out.println("Put the valid input");
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            bookManagement.loadBooks();
+        } catch (IOException e) {
+            System.out.println("Error loading books: " + e.getMessage());
+        }
+
+        int choice;
+        while ((choice = menuList()) != 0) {
+            switch (choice) {
+                case 1:
+                    bookManagement.displayAllBooks();
+                    break;
+                case 2:
+                    System.out.print("Enter ISBN to find: ");
+                    String isbnFind = sc.nextLine();
+                    Book book = bookManagement.findBook(isbnFind);
+                    if (book != null) {
+                        System.out.println(book);
+                    } else {
+                        System.out.println("Book not found.");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter ISBN: ");
+                    String isbn = sc.nextLine();
+                    System.out.print("Enter Category (SCIENCE, FICTION, HEALTH): ");
+                    Category category = Category.valueOf(sc.nextLine().toUpperCase());
+                    System.out.print("Enter Price: ");
+                    double price = sc.nextDouble();
+                    sc.nextLine(); // consume newline
+                    System.out.print("Enter Publish Date: ");
+                    String publishDate = sc.nextLine();
+                    System.out.print("Enter Author Name: ");
+                    String authorName = sc.nextLine();
+                    System.out.print("Enter Quantity: ");
+                    int quantity = sc.nextInt();
+                    sc.nextLine(); // consume newline
+                    bookManagement.addBook(new Book(isbn, category, price, publishDate, authorName, quantity));
+                    break;
+                case 4:
+                    System.out.print("Enter ISBN to remove: ");
+                    String isbnRemove = sc.nextLine();
+                    bookManagement.removeBook(isbnRemove);
+                    break;
+                case 5:
+                    System.out.print("Enter ISBN to edit quantity: ");
+                    String isbnEdit = sc.nextLine();
+                    System.out.print("Enter new quantity: ");
+                    int newQuantity = sc.nextInt();
+                    sc.nextLine(); // consume newline
+                    bookManagement.editBookQuantity(isbnEdit, newQuantity);
+                    break;
+                case 6:
+                    try {
+                        bookManagement.saveBooks();
+                    } catch (IOException e) {
+                        System.out.println("Error saving books: " + e.getMessage());
+                    }
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Put the valid input");
+                    break;
+            }
+        }
+    }
 }
